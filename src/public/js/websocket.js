be63f17012292f93cb90674fdc0e6d50a264ef93
dohoa00248 <dohoa00248@gmail.com>
2025-03-29 00:36:56 +0700

    Update new

--- src/public/js/websocket.js
+++ src/public/js/websocket.js
@@ -3,6 +3,9 @@ $(document).ready(function () {
   const ws = new WebSocket(
     "wss://iot-health-monitoring-for-the-elderly.onrender.com:443"
   );
+  ws.onopen = () => {
+    console.log("WebSocket connected");
+  };
   ws.onmessage = function (event) {
     const healthData = JSON.parse(event.data);
     console.log(healthData);
