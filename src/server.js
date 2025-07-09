46f30c557d8c9ed334155b101a0573a33065a515
dohoa00248 <dohoa00248@gmail.com>
2025-07-09 22:20:58 +0700

    setup server

--- src/server.js
+++ src/server.js
@@ -47,6 +47,6 @@ webRoutes(app);
 const PORT = process.env.PORT || 3000;
 
 // Bắt đầu server và lắng nghe kết nối
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
